5b290b2367e428f299cc35ecb04e0b2c141d161a
Rishiraj Singh Shekhawat <rsshekhawat795@gmail.com>
2021-09-01 10:49:04 +0530

    NEURAL MODEL

--- i.py
+++ i.py
@@ -21,7 +21,7 @@ data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', start, e
 
 # Prepare Data
 Scaler = MinMaxScaler(feature_range=(0,1))
-Scaler_data = Scaler.fit_transform(data['Close'].values.reshape(-,1,1))
+Scaler_data = Scaler.fit_transform(data['Close'].values.reshape(-1,1))
 
 prediction_days = 60
 
@@ -33,3 +33,18 @@ for x in range(prediction_days, len(Scaler_data)):
 
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_tain.shape[0], x_train.shape[1], 1))
+
+# Creat Neural Network
+
+model = Sequential()
+
+model.add(LSTM(unit=50, return_sequences=True, input_shape=(x_train.shape[1])))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.Dense(units=1)
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
