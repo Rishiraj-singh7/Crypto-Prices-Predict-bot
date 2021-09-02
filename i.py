b4d8f34f8675de5c45702e26efb280be1c1d7f01
Rishiraj Singh Shekhawat <rsshekhawat795@gmail.com>
2021-09-02 22:00:08 +0530

    texting model

--- i.py
+++ i.py
@@ -48,3 +48,19 @@ model.Dense(units=1)
 
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+# Testing The Model
+
+test_start = DT.datetime(2020,1,1)
+test_end = datetime.now()
+
+test_data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', test_strat, test_end)
+actual_prices = test_data['Close'].values.reshape
+
+total_dataset = pd.contact((data['Close'], test_data['Close']), axis=0)
+
+model_input = total_dataset[LEN(total_dataset)- len(test_data)- prediction_days:].values
+model_input = model_inputs.reshape(-1, 1)
+model_input = scaler.fit_transform(model_input)
+
+x_test = []
